1ff37ce03f8678d3e537c7d876da847e10592773
Tal Peer <81310989+TalPeer-github@users.noreply.github.com>
2024-12-03 00:17:37 +0200

    Update CHEV.py

--- src/models/CHEV.py
+++ src/models/CHEV.py
@@ -13,14 +13,9 @@ class ChebNet(torch.nn.Module):
     using a truncated expansion of Chebyshev polynomials.
 
     How it works:
-        Spectral convolution: In spectral graph convolution, a graph filter is applied to the graph
-            Laplacian's eigenvectors. However, direct computation of the eigenvectors is costly for large graphs.
-        Chebyshev approximation: Instead of calculating the eigenvectors, ChebNet approximates the spectral convolution
-            by using Chebyshev polynomials. These polynomials are used to approximate the graph convolution operation,
-            reducing the computational cost.
-        Chebyshev filter: The convolution operation is approximated using a truncated
-            expansion of Chebyshev polynomials of the graph Laplacian. The order of the polynomial, denoted by K,
-            determines how many terms of the expansion are used.
+        Spectral convolution: a graph filter is applied to the graph Laplacian's eigenvectors (direct computation of the eigenvectors is costly for large graphs).
+        Chebyshev approximation:  approximates the spectral convolution by using Chebyshev polynomials. These polynomials are used to approximate the graph convolution operation to reduce computational cost.
+        Chebyshev filter: The convolution operation is approximated using a truncated expansion of Chebyshev polynomials of the graph Laplacian. K - polynomial order, determines how many terms of the expansion are used.
     """
 
     def __init__(self, num_node_features, hidden_channels, num_classes, p=0.4, seed=42):
@@ -28,7 +23,7 @@ class ChebNet(torch.nn.Module):
         torch.manual_seed(seed=seed)
         self.p = p
 
-        self.conv1 = ChebConv(num_node_features, hidden_channels, K=2)  # K is the order of the polynomial
+        self.conv1 = ChebConv(num_node_features, hidden_channels, K=2)  
         self.conv2 = ChebConv(hidden_channels, hidden_channels, K=2)
         self.conv3 = ChebConv(hidden_channels, hidden_channels, K=2)
         self.lin = Linear(hidden_channels, num_classes)
@@ -40,7 +35,7 @@ class ChebNet(torch.nn.Module):
         x = x.relu()
         x = self.conv3(x, edge_index)
 
-        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+        x = global_mean_pool(x, batch)
 
         x = F.dropout(x, p=self.p, training=self.training)
         x = self.lin(x)
